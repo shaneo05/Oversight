586a6d8063d2f2b8d8d8c20cd68ce0d1a8374618
shaneo05 <32741559+shaneo05@users.noreply.github.com>
2020-01-25 22:54:13 +0000

    Adds Collector file for Solidity Contract Constructors (properties).

---
+++ Sources/ConversionToBoogie/accumulator_SOL_Constructor.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace SolToBoogie
+{
+    using SolidityAST;
+
+    /**
+     * Collect all constructor definitions and put them in the translator context.
+     */
+    public class accumulator_SOL_Constructor : BasicASTVisitor
+    {
+        private TranslatorContext context;
+
+        public accumulator_SOL_Constructor(TranslatorContext context)
+        {
+            this.context = context;
+        }
+
+        public override bool Visit(ContractDefinition node)
+        {
+            foreach (ASTNode child in node.Nodes)
+            {
+                if (child is FunctionDefinition function)
+                {
+                    if (function.IsConstructor)
+                    {
+                        context.AddConstructorToContract(node, function);
+                    }
+                    else if (function.IsFallback)
+                    {
+                        context.AddFallbackToContract(node, function);
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
