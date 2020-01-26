2f57a3e46f5b6764000dd769603406f338d1f44f
shaneo05 <32741559+shaneo05@users.noreply.github.com>
2020-01-26 19:44:10 +0000

    Adds Definition collector for sol files.

---
+++ Sources/ConversionToBoogie/accumulator_SOL_Definitions.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace SolToBoogie
+{
+    using SolidityAST;
+    using BoogieAST;
+    using System.Collections.Generic;
+    using System;
+    using System.Diagnostics;
+
+    /**
+     * Collect all using definitions and put them in the translator context.
+     */
+    public class DefinitionsAccumulator : Generic_Syntax_Tree_Visitor
+    {
+        private TranslatorContext classContext;
+        private ContractDefinition currentContract;
+        public void setContext(TranslatorContext context)
+        {
+            this.classContext = context;
+        }
+
+        public override bool Visit(ContractDefinition node)
+        {
+            currentContract = node;
+            classContext.UsingMap[currentContract] = new Dictionary<UserDefinedTypeName, TypeName>();
+            return true;
+        }
+
+        public override void EndVisit(ContractDefinition node)
+        {
+            currentContract = null;
+        }
+
+        public override bool Visit(UsingForDirective node)
+        {
+            if (node.TypeName is UserDefinedTypeName userType)
+            {
+                Debug.Assert(!userType.TypeDescriptions.IsContract(), $"OverSight does not support using A for B where B is a contract name, found {userType.ToString()}");
+            }
+            classContext.UsingMap[currentContract][node.LibraryName] = node.TypeName;
+            return true;
+        }
+    }
+}
