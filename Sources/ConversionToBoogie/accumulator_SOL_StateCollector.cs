ed73564c7e8daaaf7d22510d73e49c241cbb8249
shaneo05 <32741559+shaneo05@users.noreply.github.com>
2020-01-26 19:47:45 +0000

    add StateCollector for solidity file.

---
+++ Sources/ConversionToBoogie/accumulator_SOL_StateCollector.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace SolToBoogie
+{
+    using System.Diagnostics;
+    using SolidityAST;
+
+    public class accumulator_SOL_StateCollector : Generic_Syntax_Tree_Visitor
+    {
+        private TranslatorContext classContext;
+
+        public void setContext(TranslatorContext context)
+        {
+            this.classContext = context;
+        }
+
+        public override bool Visit(ContractDefinition node)
+        {
+            foreach (ASTNode child in node.Nodes)
+            {
+                if (child is VariableDeclaration varDecl)
+                {
+                    Debug.Assert(varDecl.StateVariable, $"{varDecl.Name} is not a state variable");
+                    // add all state variables to the context
+                    classContext.AddStateVarToContract(node, varDecl);
+                }
+            }
+            return false;
+        }
+    }
+}
