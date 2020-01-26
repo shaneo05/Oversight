4b740ed629c2f598a66ecb296418bfc27ffee1a4
shaneo05 <32741559+shaneo05@users.noreply.github.com>
2020-01-26 10:17:57 +0000

    Further removes redundancies, adds some formatting of code.

--- Sources/OverSight/OverSight_Controller.cs
+++ Sources/OverSight/OverSight_Controller.cs
@@ -12,25 +12,33 @@ namespace OverSightHandler
     using System.Collections.Generic;
     using System.Diagnostics;
     using System.IO;
-   
+
 
     internal class OverSightExecutor
     {
+        //Class Variables
+
+        //Loc of Solidity File Path
         private string SolidityFilePath;
+        //Loc of Solidity File Directory
         private string SolidityFileDir;
+
+        //Loc of Solidity Contract Name
         private string ContractName;
-        private string CorralPath;
-        private string BoogiePath;
-        private string SolcPath;
-        private bool TryProof;
-        // private bool GenInlineAttrs;
-        private ILogger Logger;
+
+        //Loc of BoogieExecutablePath
+        private string BoogieExecutablePath;
+        //Loc of SolidityCompilerPath
+        private string SolidityCompilerPath;
+
+        //Bool attempt proof value
+        private bool AttemptProof;
+
+        //BPL file containing boogie output 
         private readonly string outFileName = "BoogieConversion.bpl";
-        private readonly string corralTraceFileName = "corral_out_trace.txt";
 
         private HashSet<Tuple<string, string>> ignoreMethods;
         private TranslatorFlags translatorFlags;
-        private bool printTransactionSequence = false; 
 
         public OverSightExecutor(string solidityFilePath, string contractName, HashSet<Tuple<string, string>> ignoreMethods,bool tryProofFlag, ILogger logger, bool _printTransactionSequence, TranslatorFlags _translatorFlags = null)
         {
@@ -41,17 +49,13 @@ namespace OverSightHandler
             Console.WriteLine($"Application arguments accepted.\n");
             Console.WriteLine($"Running OverSight on Solidity Contract : {contractName}\n");
 
-            //Console.WriteLine($"SpecFilesDir = {SolidityFileDir}");
-
             //Due to no automation, the download path of the solidity and boogie compilers/exe must be manually stated
             //This is subject to change within the next week as dynamic flexibility is required across multiple machines
-            this.BoogiePath = "C:\\Users\\shane\\Desktop\\TempOversight\\bin\\Debug\\boogie.exe";
-            this.SolcPath = "C:\\Users\\shane\\Desktop\\TempOversight\\bin\\Debug\\solc.exe";
+            this.BoogieExecutablePath = "C:\\Users\\shane\\Desktop\\TempOversight\\bin\\Debug\\boogie.exe";
+            this.SolidityCompilerPath = "C:\\Users\\shane\\Desktop\\TempOversight\\bin\\Debug\\solc.exe";
             this.ignoreMethods = new HashSet<Tuple<string, string>>(ignoreMethods);
-            this.Logger = logger;
-            this.TryProof = tryProofFlag;
-            this.printTransactionSequence = _printTransactionSequence;
-            //this.GenInlineAttrs = genInlineAttrs;
+            this.AttemptProof = tryProofFlag;
+
             this.translatorFlags = _translatorFlags;
         }
 
@@ -64,7 +68,7 @@ namespace OverSightHandler
                 return 1;
             }
 
-            if (TryProof)   //if proofing is true attempt to find a proof via FindProof();
+            if (AttemptProof)   //if proofing is true attempt to find a proof via FindProof();
             {
                 if (FindProof())
                 {
@@ -102,7 +106,7 @@ namespace OverSightHandler
            
 
             //Console.WriteLine($"... running {BoogiePath} {boogieArgString}");
-            var boogieOut = RunBinary(BoogiePath, boogieArgString);
+            var boogieOut = RunBinary(BoogieExecutablePath, boogieArgString);
             var boogieOutFile = "boogie.txt";
             using (var bFile = new StreamWriter(boogieOutFile))
             {
@@ -124,9 +128,7 @@ namespace OverSightHandler
                 Console.WriteLine($"\t*** OverSight was unable to find a proof (see {boogieOutFile})");
                 return false;
             }
-            
-      
-            return false;
+                 
         }
 
      
@@ -139,7 +141,7 @@ namespace OverSightHandler
             Console.WriteLine($"Running Compiler on {ContractName}.");
 
             SolidityCompiler compiler = new SolidityCompiler();
-            CompilerOutput compilerOutput = compiler.Compile(SolcPath, SolidityFilePath);
+            CompilerOutput compilerOutput = compiler.Compile(SolidityCompilerPath, SolidityFilePath);
 
             if (compilerOutput.ContainsError())
             {
