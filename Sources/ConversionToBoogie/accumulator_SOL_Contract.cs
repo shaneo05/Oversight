b50c4f30cd6795a37cf463d0ffe69122861a370c
shaneo05 <32741559+shaneo05@users.noreply.github.com>
2020-01-26 19:43:04 +0000

    Adds Contract collector class for sol files

---
+++ Sources/ConversionToBoogie/accumulator_SOL_Contract.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace SolToBoogie
+{
+    using SolidityAST;
+
+    public class accumulator_SOL_Contract : Generic_Syntax_Tree_Visitor
+    {
+        private TranslatorContext classContext;
+
+        public override bool Visit(ContractDefinition node)
+        {
+            if(classContext != null)
+                classContext.AddContract(node);
+            return false;
+        }
+
+        //provides base context, called after instantiation
+        public void setContext(TranslatorContext context)
+        {
+            this.classContext = context;
+        }
+    }
+}
