74d790bc2a09b4d90279db0af975e5695cc1fd23
shaneo05 <32741559+shaneo05@users.noreply.github.com>
2020-01-26 19:45:17 +0000

    adds FunctionEvent collector for solidity contracts filter.

---
+++ Sources/ConversionToBoogie/accumulator_SOL_FunctionEvent.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace SolToBoogie
+{
+    using System.Diagnostics;
+    using SolidityAST;
+
+    /**
+     * Collect all function/event definitions and put them in the translator context.
+     * The result map only contains functions/events directly defined in each contract,
+     * but does not contain inherited functions/events.
+     */
+    public class accumulator_SOL_FunctionEvent : Generic_Syntax_Tree_Visitor
+    {
+        private TranslatorContext classContext;
+
+        // current contract that the visitor is visiting
+        private ContractDefinition currentContract = null;
+
+        public void setContext(TranslatorContext context)
+        {
+            this.classContext = context;
+        }
+
+        public override bool Visit(ContractDefinition node)
+        {
+            currentContract = node;
+            return true;
+        }
+
+        public override void EndVisit(ContractDefinition node)
+        {
+            currentContract = null;
+        }
+
+        public override bool Visit(EventDefinition node)
+        {
+            Debug.Assert(currentContract != null);
+            classContext.AddEventToContract(currentContract, node);
+            return false;
+        }
+
+        public override bool Visit(FunctionDefinition node)
+        {
+            Debug.Assert(currentContract != null);
+            classContext.AddFunctionToContract(currentContract, node);
+            return false;
+        }
+    }
+}
