80fc70c014538bf44980033d852af6d6a96f0b77
shaneo05 <32741559+shaneo05@users.noreply.github.com>
2020-02-02 20:31:26 +0000

    Adds dedicated resolvers for function events and state variables.

---
+++ Sources/ConversionToBoogie/OverSight_Event_Resolver.cs
@@ -0,0 +1,95 @@
+﻿
+
+namespace SolToBoogie
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using SolidityAST;
+
+    /**
+     * Determine the visible functions/events for each contract considering
+     * the inheritance hierarchy, and put the information in the context.
+     */
+    public class OverSight_Event_Resolver
+    {
+        // require the ContractDefinitions member is populated
+        // require the ContractToFunctionsMap member is populated
+        private TranslatorContext context;
+
+        public OverSight_Event_Resolver(TranslatorContext context)
+        {
+            this.context = context;
+        }
+
+        // TODO: resolve events
+        public void Resolve()
+        {
+            ResolveFunctions();
+            ComputeVisibleFunctions();
+        }
+
+        private void ResolveFunctions()
+        {
+            OverSight_GenericResolver resolutionHelper = new OverSight_GenericResolver(context);
+            List<ContractDefinition> sortedContracts = resolutionHelper.TopologicalSortByDependency(context.ContractDefinitions);
+
+            foreach (ContractDefinition contract in sortedContracts)
+            {
+                // create a deep copy
+                List<int> linearizedBaseContractIds = new List<int>(contract.LinearizedBaseContracts);
+                linearizedBaseContractIds.Reverse();
+
+                foreach (int id in linearizedBaseContractIds)
+                {
+                    ContractDefinition baseContract = context.GetASTNodeById(id) as ContractDefinition;
+                    Debug.Assert(baseContract != null);
+
+                    if (baseContract == contract)
+                    {
+                        HashSet<FunctionDefinition> functions = context.GetFuncDefintionsInContract(contract);
+                        foreach (FunctionDefinition function in functions)
+                        {
+                            string signature = TranslatorUtilities.ComputeFunctionSignature(function);
+                            context.AddFunctionToDynamicType(signature, contract, function);
+                        }
+                    }
+                    else
+                    {
+                        HashSet<FunctionDefinition> functions = context.GetFuncDefintionsInContract(baseContract);
+                        foreach (FunctionDefinition function in functions)
+                        {
+                            if (function.Visibility == EnumVisibility.PRIVATE) continue;
+
+                            string signature = TranslatorUtilities.ComputeFunctionSignature(function);
+                            context.AddFunctionToDynamicType(signature, contract, function);
+                        }
+                        // Events
+                        // TODO: Do we need to lookup by signature?
+                        HashSet<EventDefinition> events = context.GetEventDefintionsInContract(baseContract);
+                        foreach (var evt in events)
+                        {
+                            context.AddEventToContract(contract, evt);
+                        }
+                    }
+                }
+            }
+
+            // PrintFunctionResolutionMap();
+        }
+
+        private void ComputeVisibleFunctions()
+        {
+            foreach (string funcSig in context.FuncSigResolutionMap.Keys)
+            {
+                foreach (ContractDefinition contract in context.FuncSigResolutionMap[funcSig].Keys)
+                {
+                    FunctionDefinition funcDef = context.FuncSigResolutionMap[funcSig][contract];
+                    context.AddVisibleFunctionToContract(funcDef, contract);
+                }
+            }
+
+            // PrintVisibleFunctions();
+        }
+    }
+}
---
+++ Sources/ConversionToBoogie/OverSight_VariableResolver.cs
@@ -0,0 +1,109 @@
+﻿
+
+namespace SolToBoogie
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using SolidityAST;
+
+    /**
+    * Determine the visible state variables for each contract considering
+    * the inheritance hierarchy, and put the information in the context.
+    */
+    public class OverSight_VariableResolver
+    {
+        private TranslatorContext context;
+
+        public OverSight_VariableResolver(TranslatorContext context)
+        {
+            this.context = context;
+        }
+
+        public void Resolve()
+        {
+            ResolveStateVariables();
+            ComputeVisibleStateVariables();
+        }
+
+        private void ResolveStateVariables()
+        {
+            OverSight_GenericResolver resolutionHelper = new OverSight_GenericResolver(context);
+            List<ContractDefinition> sortedContracts = resolutionHelper.TopologicalSortByDependency(context.ContractDefinitions);
+
+            foreach (ContractDefinition contract in sortedContracts)
+            {
+                // create a deep copy
+                List<int> linearizedBaseContractIds = new List<int>(contract.LinearizedBaseContracts);
+                linearizedBaseContractIds.Reverse();
+
+                foreach (int id in linearizedBaseContractIds)
+                {
+                    ContractDefinition baseContract = context.GetASTNodeById(id) as ContractDefinition;
+                    Debug.Assert(baseContract != null);
+
+                    if (baseContract == contract)
+                    {
+                        HashSet<VariableDeclaration> stateVars = context.GetStateVarsByContract(contract);
+                        foreach (VariableDeclaration stateVar in stateVars)
+                        {
+                            string name = stateVar.Name;
+                            context.AddStateVarToDynamicType(name, contract, stateVar);
+                        }
+                    }
+                    else
+                    {
+                        HashSet<VariableDeclaration> stateVars = context.GetStateVarsByContract(baseContract);
+                        foreach (VariableDeclaration stateVar in stateVars)
+                        {
+                            if (stateVar.Visibility == EnumVisibility.PRIVATE) continue;
+
+                            string name = stateVar.Name;
+                            context.AddStateVarToDynamicType(name, contract, stateVar);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ComputeVisibleStateVariables()
+        {
+            foreach (string varName in context.StateVarNameResolutionMap.Keys) 
+            {
+                foreach (ContractDefinition contract in context.StateVarNameResolutionMap[varName].Keys)
+                {
+                    VariableDeclaration varDecl = context.StateVarNameResolutionMap[varName][contract];
+                    context.AddVisibleStateVarToContract(varDecl, contract);
+                }
+            }
+        }
+
+        private void PrintStateVarResolutionMap()
+        {
+            foreach (string name in context.StateVarNameResolutionMap.Keys)
+            {
+                Console.WriteLine("-- " + name);
+                foreach (ContractDefinition dynamicType in context.StateVarNameResolutionMap[name].Keys)
+                {
+                    VariableDeclaration varDecl = context.GetStateVarByDynamicType(name, dynamicType);
+                    ContractDefinition contract = context.GetContractByStateVarDecl(varDecl);
+                    Console.WriteLine(dynamicType.Name + " --> " + contract.Name + "." + varDecl.Name);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private void PrintVisibleStateVars()
+        {
+            foreach (ContractDefinition contract in context.ContractToVisibleStateVarsMap.Keys)
+            {
+                Console.WriteLine(contract.Name + ":");
+                foreach (VariableDeclaration varDecl in context.ContractToVisibleStateVarsMap[contract])
+                {
+                    Console.WriteLine(varDecl);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
